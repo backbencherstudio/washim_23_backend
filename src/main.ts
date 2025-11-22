fae8951acc17a427072f45f44e1c88b2de90e409
mdtajulislammt <mdtajulislam50599@gmail.com>
2025-11-22 11:54:50 +0600

    main confict solve

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { join } from 'path';
-<<<<<<< HEAD
-import { json, urlencoded } from 'express';
-=======
->>>>>>> 9b7bf24 (import some working)
 // import express from 'express';
 // internal imports
 import { AppModule } from './app.module';
@@ -27,12 +23,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
   app.use(helmet());
-<<<<<<< HEAD
- app.use(json({ limit: '5gb' })); 
-  app.use(urlencoded({ extended: true, limit: '5gb' }));
-  
-=======
->>>>>>> 9b7bf24 (import some working)
   // Enable it, if special charactrers not encoding perfectly
   // app.use((req, res, next) => {
   //   // Only force content-type for specific API routes, not Swagger or assets
